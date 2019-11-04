a4c0b79a3fe302944ca688d04228378c1b62a5fe
Tony Calavano <calavano@gmail.com>
2019-11-04 13:03:20 -0800

    Updated verify_checksum script
    Added options:
    * basic checking for duplicates ( -u, --duplicates)
    * output results to file ( -o, --output FILENAME)
    * option for verbose (-v, --verbose)
    * option for quiet (-q, --quiet)
    
    Improved sanity checking.
    Addded some code comments to add some clarity in code.
    Cleaned up terminal output of results.
    
    option for inscreased verbosity, option for quiet output.

--- verify_checksum.rb
+++ verify_checksum.rb
@@ -1,3 +1,16 @@
+# Checksum verification script. Verificaton is intended to detect against
+# errors in file transfers and/or other unintended bit changes. MD5 checksums
+# are used because of their low computational overhead.
+#
+# Run as 'ruby verify_checksum.rb -h' to see syntax of options.
+#
+# This script is intended for verification and deletion of content using the
+# checksum files where # each line has the following syntax:
+# "druid:[druid], [filename], [md5], [sha1], [sha256], [file_size]"
+#
+# Since the checksum files do not contain the original directory strucutre,
+# duplicate file names are ignored by default.
+
 require 'digest/md5'
 require 'find'
 require 'optparse'
@@ -20,6 +33,18 @@ OptionParser.new do |opts|
   opts.on('-d', '--delete', 'Delete files as they are verified') do |v|
     options[:delete] = v
   end
+  opts.on('-u', '--duplicates', 'Attempt to delete duplicates.') do |v|
+    options[:duplicates] = v
+  end
+  opts.on('-o', '--output FILENAME', 'Print results to csv at FILENAME.') do |v|
+    options[:output] = v
+  end
+  opts.on('-v', '--verbose', 'Print all results to terminal.') do |v|
+    options[:verbose] = v
+  end
+  opts.on('-q', '--quiet', 'Do not output to terminal.') do |v|
+    options[:quiet] = v
+  end
   opts.on('-h', '--help', 'Display this screen') do
     puts opts
     exit
@@ -30,16 +55,23 @@ OptionParser.new do |opts|
   end
 end.parse!
 
-# Some sanity checking
+# Some sanity checking on input.
 begin
-  raise 'Neet to specify both checksum file and file path.' if options[:checksum_file].nil? || options[:path].nil?
-  raise "Checksum file #{options[:checksum_file]} does not exist." unless File.exist?(options[:checksum_file])
-  raise "File path #{options[:path]} does not exist." unless File.exist?(options[:path])
+  if options[:checksum_file].nil? || options[:path].nil?
+    raise 'Neet to specify both checksum file and file path.'
+  end
+  unless File.exist?(options[:checksum_file])
+    raise "Checksum file #{options[:checksum_file]} does not exist."
+  end
+  unless File.exist?(options[:path])
+    raise "File path #{options[:path]} does not exist."
+  end
 rescue StandardError => e
   puts e
   exit
 end
 
+# Find all local files and store them in Hash.
 def get_local_files(path)
   local_files = {}
   Find.find(path) do |element|
@@ -50,6 +82,9 @@ def get_local_files(path)
 
     # There can be multiple files with the same name.
     # Store files paths in array in a hash with file name as key.
+    # hash will look like:
+    # { 'some_local_file.txt' => ['relative/path/to/some_local_file.txt',
+    #                             'relative/path/to/different/some_local_file.txt']
     if local_files.key?(basename)
       local_files[basename].push(element)
     else
@@ -59,88 +94,136 @@ def get_local_files(path)
   local_files
 end
 
+# Read DOR checksum file and put values into Hash.
 def read_checksum_file(checksum_file)
   checksums = []
   File.readlines(checksum_file).map do |line|
     line.chomp!
     values = line.split(', ')
-    info   = { 'druid'     => values.at(0),
-               'file_name' => values.at(1),
-               'md5'       => values.at(2),
-               'sha1'      => values.at(3),
-               'sha256'    => values.at(4),
-               'size'      => values.at(5) }
 
-     checksums.push(info)
+    info = { 'druid'     => values.at(0),
+             'file_name' => values.at(1),
+             'md5'       => values.at(2),
+             'sha1'      => values.at(3),
+             'sha256'    => values.at(4),
+             'size'      => values.at(5) }
+
+    checksums.push(info)
   end
   checksums
 end
 
-# Do not delete unless delete flag is explicitly defined
-delete        = !options[:delete].nil?
-
-content_dir   = options[:path]
-checksum_file = options[:checksum_file]
+# Instantiate option variables
+quiet           = !options[:quiet].nil?
+verbose         = !options[:verbose].nil?
+delete          = !options[:delete].nil?
+output          = !options[:output].nil?
+output_file     = options[:output]
+skip_duplicates = options[:duplicates].nil?
+content_dir     = options[:path]
+checksum_file   = options[:checksum_file]
 
 # Store some statistics
-# checked = 0
-valid   = []
-errors  = []
-missing = []
+valid      = []
+errors     = []
+missing    = []
+duplicates = []
+log        = nil
 
-print "Getting list of files from: #{content_dir}.. "
+if output
+  begin
+    log = open(output_file, 'a')
+  rescue StandardError => e
+    puts e
+    exit
+  end
+end
+
+print "Getting list of files from: #{content_dir}.. " unless quiet
 local_files = get_local_files(content_dir)
-print "Found: #{local_files.size}\n"
+print "Found: #{local_files.size}\n" unless quiet
 
-print "Getting list of checksums from: #{checksum_file}.. "
+print "Getting list of checksums from: #{checksum_file}.. " unless quiet
 checksums = read_checksum_file(checksum_file)
-print "Found: #{checksums.size}\n"
+print "Found: #{checksums.size}\n" unless quiet
 
-puts 'Verifying checksums:'
+puts 'Verifying checksums:' unless quiet
 checksums.each_with_index do |checksum_info, index|
-  # Check to see if file is in the checksum file, but not in the filesystem.
+
+  # Check to see if file is in the checksum manifest, but not in the filesystem.
   if local_files[checksum_info['file_name']].nil?
-    # puts "MISSING: #{file}"
     missing.push("#{checksum_info['druid']}:#{checksum_info['file_name']}")
+
+    message = "#{checksum_info['druid']},#{checksum_info['file_name']},NOT_FOUND"
+    puts "#{index + 1} of #{checksums.size},#{message}" if verbose
+    log.puts message if output
     next
   end
 
+  # Check to see if there are duplicates of filename on filesystem.
+  # Checksum manifest is only at the file-level, skip by default until 
+  # error handling can be improved. You can bypass this by using
+  # the --duplicates command-line option.
+  if local_files[checksum_info['file_name']].size >= 2 && skip_duplicates
+    duplicates.push("#{checksum_info['druid']}:#{checksum_info['file_name']}")
+
+    message = "#{checksum_info['druid']},#{checksum_info['file_name']},SKIP_DUPLICATE"
+    puts "#{index + 1} of #{checksums.size},#{message}" if verbose
+    log.puts message if output
+    next
+  end
+
+  # Attempt to find a local file that matches the checksum in the digest manifest.
   matched      = false
   matched_file = ''
   local_files[checksum_info['file_name']].each do |local_file|
+    next unless File.exist?(local_file)
+
     local_file_md5 = Digest::MD5.hexdigest(File.read(local_file))
+    next unless checksum_info['md5'].eql?(local_file_md5)
 
-    # Useful for debugging
-    # puts "\nlocalfile: " + local_file_md5
-    # puts "cehcksum:  " + checksum_info['md5']
-
-    if checksum_info['md5'].eql?(local_file_md5)
-      matched_file = local_file
-      matched = true
-      break
-    end
+    matched = true
+    matched_file = local_file
+    break
   end
 
   if matched
-    print "\r" + "\e[2K"
-    print "(#{index + 1} of #{checksums.size}) OK: #{matched_file}"
     valid.push(matched_file)
-    print ' ...unlinking' if delete
-    STDOUT.flush
     File.unlink(matched_file) if delete && File.exist?(matched_file)
 
+    message = "#{checksum_info['druid']},#{checksum_info['file_name']},OK"
+    message += ',DELETED' if delete
+
+    puts "#{index + 1} of #{checksums.size},#{message}" if verbose
+    log.puts message if output
+    # The "\r\e[2K" in the print will return to beginning of line (\r)
+    # and will clear the rest of the line (\e[2K)
+    print "\r\e[2K" + "(#{index + 1} of #{checksums.size}): #{message}" if !quiet && !verbose
+    print ' ...unlinking' if delete && !quiet && !verbose
+    STDOUT.flush
+
   else
-    print "\nERROR: #{checksum_info['druid']}:#{checksum_info['file_name']} -- No Match"
     errors.push("#{checksum_info['druid']}:#{checksum_info['file_name']}")
+
+    message = "#{checksum_info['druid']},#{checksum_info['file_name']},ERROR"
+    puts "#{index + 1} of #{checksums.size},#{message}" if verbose
+    log.puts message if output
 
   end
 end
 
-puts ''
-puts ''
-puts "Filesystem: #{local_files.size}, Digest List: #{checksums.size}"
-puts 'Results:'
-puts "OK:      #{valid.size}"
-puts "FAIL:    #{errors.size}"
-puts "MISSING: #{missing.size}"
-puts "Not in Digest List: #{local_files.size - (valid.size + errors.size)}"
+# A brief summary of the results.
+unless quiet
+  puts "\n\nSummary:"
+  puts ''
+  puts "Filesystem : #{local_files.size}"
+  puts "Digest List: #{checksums.size}"
+  puts 'Results:'
+  puts "OK  :       #{valid.size}"
+  puts "FAIL:       #{errors.size}"
+  puts "DUPLICATES: #{duplicates.size}" 
+  puts ''
+  puts "Not on disk       : #{missing.size}"
+  puts "Not in digest list: #{local_files.size - (valid.size + errors.size + duplicates.size)}"
+end
+
